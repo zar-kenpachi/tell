440c68d14a42a859e90d16cff3263b5828a0443e
zar-kenpachi <cekina03@gmail.com>
2013-05-23 15:15:22 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TellApp::Application.config.secret_token = 'a5dfb0e009092ff3c0448598a6c36b46397112806f7213abf9b0d5ca442fb6b6d7991e420d774772dd8b2306729ae02bd43f42537fe2a47ea7ce31dcf9798223'
